7f4709b7eac79c433027afd2efd68ffb4eecddfd
divyanshsahu1 <82767198+divyanshsahu1@users.noreply.github.com>
2021-07-18 16:15:25 +0530

    Add files via upload

---
+++ Geologist-Template-main/Hammer.js
@@ -0,0 +1,31 @@
+class Hammer {
+    constructor(x, y) {
+      var options = {
+        'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      };
+      this.body = Bodies.rectangle(x, y, 150, 40, options);
+      this.width = 150;
+      this.height = 40;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER)
+      strokeWeight(3);
+      stroke('white')
+      fill('orange')
+     
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
